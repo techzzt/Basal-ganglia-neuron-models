f357c661dc8a0ecede1e3ebf1f66749a6d926f18
Keun <49440852+techzzt@users.noreply.github.com>
2024-08-06 18:23:30 +0900

    Add files via upload

--- bg_insilico/simulation.py
+++ bg_insilico/simulation.py
@@ -1,65 +1,63 @@
-from brian2 import *
+from brian2 import Network, StateMonitor, SpikeMonitor, PopulationRateMonitor
 import matplotlib.pyplot as plt
 
-class Simulation:
-    def __init__(self, neuron_model, synapse_model=None):
+class Visualization:
+    def __init__(self, neuron_model):
         self.neuron_model = neuron_model
-        self.synapse_model = synapse_model
-        self.setup_monitors()
-        self.network = Network(self.neuron_model.neurons, self.dv_monitor, self.spike_monitor, self.rate_monitor)
-        if self.synapse_model:
-            self.network.add(self.synapse_model.synapses)
+        self.network = Network(neuron_model.neurons)
+        
+        # 기본 모니터 설정
+        self.dv_monitor = StateMonitor(neuron_model.neurons, 'v', record=True)
+        self.spike_monitor = SpikeMonitor(neuron_model.neurons)
+        self.rate_monitor = PopulationRateMonitor(neuron_model.neurons)
+        self.current_monitor = StateMonitor(neuron_model.neurons, 'I', record=True)
+        
+        self.network.add(self.dv_monitor, self.spike_monitor, self.rate_monitor, self.current_monitor)
 
-    def setup_monitors(self):
-        self.dv_monitor = StateMonitor(self.neuron_model.neurons, 'v', record=True)
-        self.spike_monitor = SpikeMonitor(self.neuron_model.neurons)
-        self.rate_monitor = PopulationRateMonitor(self.neuron_model.neurons)
-
-    def build_network(self):
-        self.network = Network(self.neuron_model.neurons, self.dv_monitor, self.spike_monitor, self.rate_monitor)
-        if self.synapse_model:
-            self.network.add(self.synapse_model.synapses)
-
-    def run(self, duration, plot=False, earliest_time_stabilized=None):
+    def run(self, duration):
         self.network.run(duration)
-        if plot:
-            self.plot_results(earliest_time_stabilized)
-
+    
     def plot_results(self, earliest_time_stabilized=None):
+        plt.figure(figsize=(15, 15))
 
-        plt.figure(figsize=(12, 8))
-        total_simulation_time = self.dv_monitor.t[-1]  # Get the last recorded time
-
-        plt.subplot(3, 1, 1)
-        plt.plot(self.dv_monitor.t / ms, self.dv_monitor.v[0] / mV, label='Membrane Potential')
-        if earliest_time_stabilized is not None and earliest_time_stabilized <= total_simulation_time:
-                plt.axvline(x=earliest_time_stabilized / ms, color='gray', linestyle='--', label='Stabilization')
-            
-        plt.xlim(0, total_simulation_time / ms)  # Limit x-axis to the duration of the simulation
+        # Membrane potential
+        plt.subplot(4, 1, 1)
+        plt.plot(self.dv_monitor.t / ms, self.dv_monitor.v_m[0] / mV, label='Membrane Potential')
+        if earliest_time_stabilized:
+            plt.axvline(x=earliest_time_stabilized / ms, color='gray', linestyle='--', label='Stabilization')
         plt.xlabel('Time (ms)')
         plt.ylabel('Membrane Potential (mV)')
         plt.legend()
 
-        # Plot spikes
-        plt.subplot(3, 1, 2)
-        if len(self.spike_monitor.i) > 0:
-            spike_times_of_neuron_0 = self.spike_monitor.t[self.spike_monitor.i == 0]
-            plt.plot(spike_times_of_neuron_0 / ms, np.zeros_like(spike_times_of_neuron_0), 'o', markersize=2, label='Spikes (Neuron 0)')
-        else:
-            plt.text(0.5, 0.5, 'No spikes recorded', fontsize=12, ha='center', va='center')
+        # Spikes
+        plt.subplot(4, 1, 2)
+        spikes_of_neuron_0 = self.spike_monitor.i[self.spike_monitor.i == 0] 
+        spike_times_of_neuron_0 = self.spike_monitor.t[self.spike_monitor.i == 0]  
+        plt.plot(spike_times_of_neuron_0 / ms, spikes_of_neuron_0, '.', markersize=2, label='Spikes (Neuron 0)')
         plt.xlabel('Time (ms)')
         plt.ylabel('Neuron Index (0)')
-        plt.yticks([0])
+        plt.yticks([0])  # y축에 0만 표시
         plt.legend()
-        
-        # Plot firing rate
-        plt.subplot(3, 1, 3)
-        if len(self.rate_monitor.t) > 0:
-            plt.plot(self.rate_monitor.t / ms, self.rate_monitor.smooth_rate(width=10*ms) / Hz, label='Firing Rate')
-        else:
-            plt.text(0.5, 0.5, 'No firing rate data', fontsize=12, ha='center', va='center')
+
+        # Firing rate
+        plt.subplot(4, 1, 3)
+        plt.plot(self.rate_monitor.t / ms, self.rate_monitor.smooth_rate(width=10*ms) / Hz, label='Firing Rate')
         plt.xlabel('Time (ms)')
         plt.ylabel('Firing Rate (Hz)')
+        plt.legend()
+
+        # Input current
+        plt.subplot(4, 1, 4)
+        initial_time = np.arange(0, 1000, 1) 
+        initial_current = np.zeros_like(initial_time)  
+        total_time = np.concatenate((initial_time, self.current_monitor.t / ms))
+        total_current = np.concatenate((initial_current, self.current_monitor.I[0] / pA))
+
+        # Plot the current
+        plt.plot(total_time, total_current, label='Current I (pA)', color='orange')
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Current (pA)')
+        plt.xlim(0, (self.network.clock.t / ms)) 
         plt.legend()
 
         plt.tight_layout()
