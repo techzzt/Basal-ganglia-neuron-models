353927633cad9175e2920429c56bd3ec9652f741
Keun <49440852+techzzt@users.noreply.github.com>
2024-08-08 13:53:29 +0900

    Add files via upload

--- bg_insilico/simulation_runner.py
+++ bg_insilico/simulation_runner.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from brian2 import mV, pA, siemens, ms, farad, second, pF, nS, Hz
+from brian2 import Network, StateMonitor, SpikeMonitor, PopulationRateMonitor
 import importlib
 from result import Visualization
 
@@ -49,9 +50,15 @@ def run_simulation(N, params, model_name):
     print("Converted parameters:", converted_params)  # 디버깅 출력
     
     # 뉴런 모델 초기화
+   # 초기 I 값을 0으로 설정하고 이후 사용을 위해 원래 I 값을 저장
+    initial_I = 0 * pA
+    increase_I = converted_params['I']
+    converted_params['I'] = initial_I
+    
+    # 뉴런 모델 초기화
     neuron_model = neuron_model_class(N, converted_params)
     sim = Visualization(neuron_model)
-    
+
     # 시뮬레이션 초기 실행
     Initialize_time = 1000 * ms
     sim.run(duration=Initialize_time)
@@ -66,5 +73,34 @@ def run_simulation(N, params, model_name):
         earliest_time_stabilized = times[matching_indices[0]] * 1000
     else:
         earliest_time_stabilized = None
+
+    # 새 모니터 설정
+    dv_monitor_new = StateMonitor(neuron_model.neurons, variables='v', record=True)
+    spike_monitor_new = SpikeMonitor(neuron_model.neurons)
+    rate_monitor_new = PopulationRateMonitor(neuron_model.neurons)  # 10ms의 bin 크기
+    current_monitor = StateMonitor(neuron_model.neurons, variables='I', record=True)
+
+    sim.network.add(dv_monitor_new, spike_monitor_new, rate_monitor_new, current_monitor)
+
+    # Input current
+    if earliest_time_stabilized is not None:
+        wait_time_after_stabilization = 100 * ms
+        time_after_increase = 200 * ms
+        time_after_decrease = 200 * ms
+        total_simulation_time = Initialize_time + wait_time_after_stabilization + time_after_increase + time_after_decrease
+
+        # Stabilization period
+        sim.run(duration=wait_time_after_stabilization)
+        
+        # Increase I to the value specified in JSON
+        neuron_model.neurons.I = increase_I
+        sim.run(duration=time_after_increase)
+        
+        # Decrease I back to 0
+        neuron_model.neurons.I = 0 * pA
+        sim.run(duration=time_after_decrease)
+    else:
+        print("v did not reach v_reset, stopping simulation")
+        total_simulation_time = Initialize_time
 
     return sim
