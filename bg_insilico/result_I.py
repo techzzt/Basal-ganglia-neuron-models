14f7f4c80b7688c90e5061352a0b90c5e2aacbb9
Keun <49440852+techzzt@users.noreply.github.com>
2024-08-14 17:44:47 +0900

    Add files via upload

--- bg_insilico/result_I.py
+++ bg_insilico/result_I.py
@@ -4,7 +4,7 @@ from brian2 import ms, pA
 from brian2 import Network, StateMonitor, SpikeMonitor, PopulationRateMonitor
 
 
-class Visualization:
+class Run:
     def __init__(self, neuron_model):
         self.neuron_model = neuron_model
         self.network = Network(neuron_model.neurons)
@@ -26,22 +26,3 @@ class Visualization:
         # 시뮬레이션 후에 멤브레인 전위와 입력 전류를 저장
         self.membrane_potentials.append(self.dv_monitor.v[0])
         self.input_currents.append(self.current_monitor.I[0])
-
-    def plot_results(self):
-        plt.figure(figsize=(15, 15))
-
-        for i, (membrane_potential, input_current) in enumerate(zip(self.membrane_potentials, self.input_currents)):
-            plt.subplot(10, 2, i*2 + 1)
-            plt.plot(self.dv_monitor.t / ms, membrane_potential / mV, label=f'Membrane Potential (I = {-90 + 30*i} pA)')
-            plt.xlabel('Time (ms)')
-            plt.ylabel('Membrane Potential (mV)')
-            plt.legend()
-
-            plt.subplot(10, 2, i*2 + 2)
-            plt.plot(self.current_monitor.t / ms, input_current / pA, label=f'Input Current (I = {-90 + 30*i} pA)', color='orange')
-            plt.xlabel('Time (ms)')
-            plt.ylabel('Current (pA)')
-            plt.legend()
-
-        plt.tight_layout()
-        plt.show()
