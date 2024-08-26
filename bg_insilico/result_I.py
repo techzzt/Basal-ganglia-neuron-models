42d05f5c10e37f3c943245648e08adc493ae93ee
Keun <49440852+techzzt@users.noreply.github.com>
2024-08-26 15:18:01 +0900

    Add files via upload

--- bg_insilico/result_I.py
+++ bg_insilico/result_I.py
@@ -7,19 +7,25 @@ class Run:
     def __init__(self, neuron_model):
         self.neuron_model = neuron_model
         self.network = Network(neuron_model.neurons)
+        self.monitors_set_up = False
 
     def setup_monitors(self):
-        # Setting up the monitors for each simulation run
-        self.dv_monitor = StateMonitor(self.neuron_model.neurons, 'v', record=True)
-        self.spike_monitor = SpikeMonitor(self.neuron_model.neurons)
-        self.rate_monitor = PopulationRateMonitor(self.neuron_model.neurons)
-        self.current_monitor = StateMonitor(self.neuron_model.neurons, 'I', record=True)
-        
-        self.network.add(self.dv_monitor, self.spike_monitor, self.rate_monitor, self.current_monitor)
+        if not self.monitors_set_up:
+            print("Setting up monitors")
+            self.dv_monitor = StateMonitor(self.neuron_model.neurons, 'v', record=True)
+            self.spike_monitor = SpikeMonitor(self.neuron_model.neurons)
+            self.rate_monitor = PopulationRateMonitor(self.neuron_model.neurons)
+            self.current_monitor = StateMonitor(self.neuron_model.neurons, 'I', record=True)
+
+            self.network.add(self.dv_monitor, self.spike_monitor, self.rate_monitor, self.current_monitor)
+            self.monitors_set_up = True
+        else:
+            print("Monitors already set up")
 
     def run(self, duration):
-        # Reset monitors before running to avoid accumulating data
+        print(f"Running simulation for {duration} ms")
         self.setup_monitors()
         self.network.run(duration)
-
+        # Reset the monitors for the next phase
+        self.monitors_set_up = False
 
