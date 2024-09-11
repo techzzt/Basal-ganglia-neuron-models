fb3fb0bc18609f1da6c7519f4437c7117dd06fcb
Keun <49440852+techzzt@users.noreply.github.com>
2024-09-11 16:37:22 +0900

    Add files via upload

--- bg_insilico/simulation_runner_syn.py
+++ bg_insilico/simulation_runner_syn.py
@@ -2,6 +2,7 @@ import json
 from brian2 import *
 from Neuronmodels.GPe_STN import GPeModel, STNModel, GPeSTNSynapse
 import matplotlib.pyplot as plt
+import importlib
 
 def load_params(json_file):
     with open(json_file, 'r') as f:
@@ -36,32 +37,39 @@ def convert_units(params):
         converted_params[param] = value
     return converted_params
 
-def run_simulation(gpe_params_file, stn_params_file, synapse_params):
-    # Load GPe and STN parameters from the JSON files
-    N_GPe, gpe_params, gpe_model_name = load_params(gpe_params_file)
-    N_STN, stn_params, stn_model_name = load_params(stn_params_file)
+def run_simulation(params_file_1, params_file_2, synapse_params, model_class_1, model_class_2, synapse_class):
+    
+    # Load parameters for the two neuron groups
+    N_1, params_1, model_name_1 = load_params(params_file_1)
+    N_2, params_2, model_name_2 = load_params(params_file_2)
 
     # Convert units for the neuron models
-    gpe_params_converted = convert_units(gpe_params)
-    stn_params_converted = convert_units(stn_params)
+    params_1_converted = convert_units(params_1)
+    params_2_converted = convert_units(params_2)
+    
+    # Dynamically load neuron models using the provided class names
+    model_module_1 = importlib.import_module(f'Neuronmodels.{model_class_1}')
+    model_module_2 = importlib.import_module(f'Neuronmodels.{model_class_2}')
     
     # Initialize the neuron models
-    gpe_model = GPeModel(N=N_GPe, params=gpe_params_converted)
-    stn_model = STNModel(N=N_STN, params=stn_params_converted)
+    neuron_model_1 = getattr(model_module_1, model_class_1)(N=N_1, params=params_1_converted)
+    neuron_model_2 = getattr(model_module_2, model_class_2)(N=N_2, params=params_2_converted)
 
-    GPe = gpe_model.create_neurons()
-    STN = stn_model.create_neurons()
+    neurons_1 = neuron_model_1.create_neurons()
+    neurons_2 = neuron_model_2.create_neurons()
 
-    # Set up the synapses between GPe and STN
-    synapse = GPeSTNSynapse(GPe, STN, synapse_params)
-    syn_GPe_STN = synapse.create_synapse()
+    synapse_module = importlib.import_module(f'Neuronmodels.{synapse_class}')
+
+    # Set up the synapses between the two neuron groups
+    synapse = synapse_module.GPeSTNSynapse(neurons_1, neurons_2, synapse_params)
+    syn_generic = synapse.create_synapse()
 
     # Set up the monitors
-    dv_monitor = StateMonitor(STN, 'v', record=True)
-    spike_monitor = SpikeMonitor(STN)
+    dv_monitor = StateMonitor(neurons_2, 'v', record=True)
+    spike_monitor = SpikeMonitor(neurons_2)
 
     # Create a network and run the simulation
-    net = Network(GPe, STN, syn_GPe_STN, dv_monitor, spike_monitor)
+    net = Network(neurons_1, neurons_2, syn_generic, dv_monitor, spike_monitor)
     net.run(1000*ms)
 
     # Return results for plotting
