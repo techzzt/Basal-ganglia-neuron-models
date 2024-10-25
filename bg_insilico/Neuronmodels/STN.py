1e36cd52f5ad9a26775d40bb03238ed1c43b5e2e
Keun <49440852+techzzt@users.noreply.github.com>
2024-10-25 16:15:48 +0900

    Add files via upload

--- bg_insilico/Neuronmodels/STN.py
+++ bg_insilico/Neuronmodels/STN.py
@@ -34,18 +34,41 @@ class NeuronModel:
 
     def create_neurons(self):
         raise NotImplementedError("Subclasses should implement this method.")
+    
 
-class STN(NeuronModel):
+class NeuronModel:
     def __init__(self, N, params, neuron_type='E'):
         # Parse the parameters from the params dictionary
+        super().__init__(N, params)
+        self.neuron_type = neuron_type  # Store neuron type ('E' or 'I')
+        self.neurons = None
+
+    def create_neurons(self):
+        raise NotImplementedError("Subclasses should implement this method.")
+    
+class STN(NeuronModel):
+    def __init__(self, N, params, neuron_type='E'):
         self.N = N
         self.params = params
         self.neuron_type = neuron_type  # 'E' for excitatory, 'I' for inhibitory
         self.neurons = None
 
-    def create_neurons(self):
+        @network_operation(dt=defaultclock.dt)
+        def update_v():
+            vr = self.params['vr']
+            v = self.neurons.v
+            u = self.neurons.u
 
-        # Create the neuron group using the appropriate equations
+            for i in range(len(v)):
+                if u[i] < 0*nA:
+                    v[i] = clip(v[i], vr - 15*mV, 20*mV)
+                else:
+                    v[i] = vr
+
+        # 네트워크 오퍼레이션을 현재 네트워크에 추가
+        self.update_v_op = update_v
+    
+    def create_neurons(self):
         self.neurons = NeuronGroup(self.N, eqs, threshold='v >= th', reset='v = vr; u += d', method='euler')
         for param, value in self.params.items():
             setattr(self.neurons, param, value)
