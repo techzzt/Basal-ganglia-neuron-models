c287910a2f1fb9a2d98a9da325b06724d9275515
Keun <49440852+techzzt@users.noreply.github.com>
2024-12-05 18:28:51 +0900

    Add files via upload

--- bg_insilico/module/simulation/runner.py
+++ bg_insilico/module/simulation/runner.py
@@ -4,13 +4,12 @@ from module.models.synapse_models import create_synapses
 from module.utils.data_handler import plot_raster
 
 def run_simulation_with_inh_ext_input(neuron_configs, synapse_params, synapse_class, cortex_inputs, simulation_duration=1000*ms):
-
     try:
+        start_scope()
         neuron_groups = create_neurons(neuron_configs)
         print(f"Neuron Groups: {neuron_groups.keys()}") 
 
         synapse_connections = create_synapses(neuron_groups, synapse_params, synapse_class)
-        start_scope()
 
         net = Network()
         net.add(neuron_groups.values())
@@ -28,7 +27,11 @@ def run_simulation_with_inh_ext_input(neuron_configs, synapse_params, synapse_cl
                 voltage_mon = StateMonitor(group, 'v', record=True)
                 voltage_monitors[name] = voltage_mon
                 net.add(voltage_mon)
-            
+        
+        for t in range(0, int(simulation_duration/ms), 100):  
+            print(f"Remaining time: {simulation_duration/ms - t} ms")
+            net.run(100 * ms)    
+        
         net.run(1000 * ms)
         plot_raster(spike_monitors)  
         
