23aa9c63b6c1377bcea2330c99c9fce0eb9bb026
Keun <49440852+techzzt@users.noreply.github.com>
2025-01-06 13:39:23 +0900

    Add files via upload

--- bg_insilico/Neuronmodels/GPe_STN_inh_ext_dop_dep.py
+++ bg_insilico/Neuronmodels/GPe_STN_inh_ext_dop_dep.py
@@ -33,8 +33,9 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
                 E_GABA : volt
                 w : 1
                 tau_GABA : second
+                gaba_beta : 1
                 dg_g/dt = -g_g / tau_GABA : siemens (clock-driven)
-                I_GABA_syn = w * g_g * (E_GABA - v) : amp
+                I_GABA_syn = gaba_beta * w * g_g * (E_GABA - v) : amp
             '''
         }
 
@@ -48,7 +49,6 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
             delay=self._get_param('csfs_delay'))
         syn_Cortex_FSN.connect(p = 0.75)
         syn_Cortex_FSN.w = 1
-        syn_Cortex_FSN.ampa_beta = self._get_param('ampa_beta')
         syn_Cortex_FSN.g0_a = self._get_param('csfs_g0_a')
         syn_Cortex_FSN.tau_AMPA = self._get_param('csfs_ampa_tau_syn')
         syn_Cortex_FSN.E_AMPA = self._get_param('csfs_ampa_E_rev')
@@ -62,9 +62,9 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
             ''',
         )
         syn_Cortex_MSND1_ampa.connect(p=0.75)
-
+        syn_Cortex_MSND1_ampa.w = 1
         # Assign AMPA parameters after connecting
-        syn_Cortex_MSND1_ampa.ampa_beta = self._get_param('ampa_beta')
+        syn_Cortex_MSND1_ampa.ampa_beta = self._get_param('cs1_ampa_beta')
         syn_Cortex_MSND1_ampa.g0_a = self._get_param('cs1_g0_a')
         syn_Cortex_MSND1_ampa.tau_AMPA = self._get_param('cs1_ampa_tau_syn')
         syn_Cortex_MSND1_ampa.E_AMPA = self._get_param('cs1_ampa_E_rev')
@@ -82,7 +82,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_Cortex_MSND1_nmda.connect(p=0.75)
 
         # Assign NMDA parameters after connecting
-        syn_Cortex_MSND1_nmda.nmda_beta = self._get_param('nmda_beta')
+        syn_Cortex_MSND1_nmda.nmda_beta = self._get_param('cs1_nmda_beta')
         syn_Cortex_MSND1_nmda.g0_n = self._get_param('cs1_g0_n')
         syn_Cortex_MSND1_nmda.tau_NMDA = self._get_param('cs1_nmda_tau_syn')
         syn_Cortex_MSND1_nmda.E_NMDA = self._get_param('cs1_nmda_E_rev')
@@ -98,7 +98,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
             ''', 
         )
         syn_Cortex_MSND2_ampa.connect(p=0.75)
-        syn_Cortex_MSND2_ampa.ampa_beta = self._get_param('ampa_beta')
+        syn_Cortex_MSND2_ampa.ampa_beta = self._get_param('cs2_ampa_beta')
         syn_Cortex_MSND2_ampa.g0_a = self._get_param('cs2_g0_a')
         syn_Cortex_MSND2_ampa.tau_AMPA = self._get_param('cs2_ampa_tau_syn')
         syn_Cortex_MSND2_ampa.E_AMPA = self._get_param('cs2_ampa_E_rev')
@@ -112,7 +112,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
             ''', 
         )
         syn_Cortex_MSND2_nmda.connect(p=0.75)
-        syn_Cortex_MSND2_nmda.nmda_beta = self._get_param('nmda_beta')
+        syn_Cortex_MSND2_nmda.nmda_beta = self._get_param('cs2_nmda_beta')
         syn_Cortex_MSND2_nmda.g0_n = self._get_param('cs2_g0_n')
         syn_Cortex_MSND2_nmda.tau_NMDA = self._get_param('cs2_nmda_tau_syn')
         syn_Cortex_MSND2_nmda.E_NMDA = self._get_param('cs2_nmda_E_rev')
@@ -128,7 +128,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
             delay=self._get_param('csn_delay')
         )
         syn_Cortex_STN_ampa.connect(p=0.75)
-        syn_Cortex_STN_ampa.ampa_beta = self._get_param('ampa_beta')
+        syn_Cortex_STN_ampa.ampa_beta = self._get_param('csn_ampa_beta')
         syn_Cortex_STN_ampa.g0_a = self._get_param('csn_g0_a')
         syn_Cortex_STN_ampa.tau_AMPA = self._get_param('csn_ampa_tau_syn')
         syn_Cortex_STN_ampa.E_AMPA = self._get_param('csn_ampa_E_rev')
@@ -143,7 +143,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
             delay=self._get_param('csn_delay')
         )
         syn_Cortex_STN_nmda.connect(p=0.75)
-        syn_Cortex_STN_nmda.nmda_beta = self._get_param('nmda_beta')
+        syn_Cortex_STN_nmda.nmda_beta = self._get_param('csn_nmda_beta')
         syn_Cortex_STN_nmda.g0_n = self._get_param('csn_g0_n')
         syn_Cortex_STN_nmda.tau_NMDA = self._get_param('csn_nmda_tau_syn')
         syn_Cortex_STN_nmda.E_NMDA = self._get_param('csn_nmda_E_rev')
@@ -160,6 +160,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         )
         syn_FSN_FSN.connect(p=0.74)
         syn_FSN_FSN.g0_g = self._get_param('fsfs_g0_g')
+        syn_FSN_FSN.gaba_beta = self._get_param('fsfs_gaba_beta')
         syn_FSN_FSN.tau_GABA = self._get_param('fsfs_gaba_tau_syn')
         syn_FSN_FSN.E_GABA = self._get_param('fsfs_gaba_E_rev')
         synapses.append(syn_FSN_FSN)
@@ -205,6 +206,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         )
         syn_MSND1_SNr.connect(p=0.1)
         syn_MSND1_SNr.g0_g = self._get_param('d1snr_g0_g')
+        syn_MSND1_SNr.gaba_beta = self._get_param('d1snr_gaba_beta')
         syn_MSND1_SNr.tau_GABA = self._get_param('d1snr_gaba_tau_syn')
         syn_MSND1_SNr.E_GABA = self._get_param('d1snr_gaba_E_rev')
         synapses.append(syn_MSND1_SNr)
@@ -215,6 +217,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_MSND1_MSND1.connect(p=0.18)
         syn_MSND1_MSND1.w = 1
         syn_MSND1_MSND1.g0_g = self._get_param('d1d1_g0_g')
+        syn_MSND1_MSND1.gaba_beta = self._get_param('d1d1_gaba_beta')
         syn_MSND1_MSND1.tau_GABA = self._get_param('d1d1_gaba_tau_syn')
         syn_MSND1_MSND1.E_GABA = self._get_param('d1d1_gaba_E_rev')
         synapses.append(syn_MSND1_MSND1)
@@ -225,6 +228,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_MSND1_MSND2.connect(p=0.03)
         syn_MSND1_MSND2.w = 1
         syn_MSND1_MSND2.g0_g = self._get_param('d1d2_g0_g')
+        syn_MSND1_MSND2.gaba_beta = self._get_param('d1d2_gaba_beta')
         syn_MSND1_MSND2.tau_GABA = self._get_param('d1d2_gaba_tau_syn')
         syn_MSND1_MSND2.E_GABA = self._get_param('d1d2_gaba_E_rev')
         synapses.append(syn_MSND1_MSND2)
@@ -235,6 +239,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_MSND2_MSND2.connect(p=0.18)
         syn_MSND2_MSND2.w = 1
         syn_MSND2_MSND2.g0_g = self._get_param('d2d2_g0_g')
+        syn_MSND2_MSND2.gaba_beta = self._get_param('d2d2_gaba_beta')
         syn_MSND2_MSND2.tau_GABA = self._get_param('d2d2_gaba_tau_syn')
         syn_MSND2_MSND2.E_GABA = self._get_param('d2d2_gaba_E_rev')
         synapses.append(syn_MSND2_MSND2)
@@ -245,6 +250,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_MSND2_MSND1.connect(p=0.14)
         syn_MSND2_MSND1.w = 1
         syn_MSND2_MSND1.g0_g = self._get_param('d2d1_g0_g')
+        syn_MSND2_MSND1.gaba_beta = self._get_param('d2d1_gaba_beta')
         syn_MSND2_MSND1.tau_GABA = self._get_param('d2d1_gaba_tau_syn')
         syn_MSND2_MSND1.E_GABA = self._get_param('d2d1_gaba_E_rev')
         synapses.append(syn_MSND2_MSND1)
@@ -255,6 +261,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_MSND2_GPeT1.connect(p=0.2)
         syn_MSND2_GPeT1.w = 1
         syn_MSND2_GPeT1.g0_g = self._get_param('d2g1_g0_g')
+        syn_MSND2_GPeT1.gaba_beta = self._get_param('d2g1_gaba_beta')
         syn_MSND2_GPeT1.tau_GABA = self._get_param('d2g1_gaba_tau_syn')
         syn_MSND2_GPeT1.E_GABA = self._get_param('d2g1_gaba_E_rev')
         synapses.append(syn_MSND2_GPeT1)
@@ -267,6 +274,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_STN_GPeT1.connect(p=0.1)
         syn_STN_GPeT1.w = 1
         syn_STN_GPeT1.g0_a = self._get_param('snt1_g0_a')
+        syn_STN_GPeT1.ampa_beta = self._get_param('snt1_ampa_beta')
         syn_STN_GPeT1.tau_AMPA = self._get_param('snt1_ampa_tau_syn')
         syn_STN_GPeT1.E_AMPA = self._get_param('snt1_ampa_E_rev')
         synapses.append(syn_STN_GPeT1)
@@ -278,6 +286,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
             ''')
         syn_STN_GPeTA.connect(p=0.1)
         syn_STN_GPeTA.w = 1
+        syn_STN_GPeTA.ampa_beta = self._get_param('snta_ampa_beta')
         syn_STN_GPeTA.g0_a = self._get_param('snta_g0_a')
         syn_STN_GPeTA.tau_AMPA = self._get_param('snta_ampa_tau_syn')
         syn_STN_GPeTA.E_AMPA = self._get_param('snta_ampa_E_rev')
@@ -291,6 +300,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_STN_SNr.connect(p=0.1)
         syn_STN_SNr.w = 1
         syn_STN_SNr.g0_a = self._get_param('snsnr_g0_a')
+        syn_STN_SNr.ampa_beta = self._get_param('snsnr_ampa_beta')
         syn_STN_SNr.tau_AMPA = self._get_param('snsnr_ampa_tau_syn')
         syn_STN_SNr.E_AMPA = self._get_param('snsnr_ampa_E_rev')
         synapses.append(syn_STN_SNr)
@@ -303,6 +313,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeT1_FSN.connect(p=0.01)
         syn_GPeT1_FSN.w = 1
         syn_GPeT1_FSN.g0_g = self._get_param('g1fs_g0_g')
+        syn_GPeT1_FSN.gaba_beta = self._get_param('g1fs_gaba_beta')
         syn_GPeT1_FSN.tau_GABA = self._get_param('g1fs_gaba_tau_syn')
         syn_GPeT1_FSN.E_GABA = self._get_param('g1fs_gaba_E_rev')
         synapses.append(syn_GPeT1_FSN)
@@ -315,6 +326,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeT1_STN.connect(p=0.03)
         syn_GPeT1_STN.w = 1
         syn_GPeT1_STN.g0_g = self._get_param('g1sn_g0_g')
+        syn_GPeT1_STN.gaba_beta = self._get_param('g1sn_gaba_beta')
         syn_GPeT1_STN.tau_GABA = self._get_param('g1sn_gaba_tau_syn')
         syn_GPeT1_STN.E_GABA = self._get_param('g1sn_gaba_E_rev')
         synapses.append(syn_GPeT1_STN)
@@ -326,6 +338,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeT1_SNr.connect(p=0.03)
         syn_GPeT1_SNr.w = 1
         syn_GPeT1_SNr.g0_g = self._get_param('g1snr_g0_g')
+        syn_GPeT1_SNr.gaba_beta = self._get_param('g1snr_gaba_beta')
         syn_GPeT1_SNr.tau_GABA = self._get_param('g1snr_gaba_tau_syn')
         syn_GPeT1_SNr.E_GABA = self._get_param('g1snr_gaba_E_rev')
         synapses.append(syn_GPeT1_SNr)
@@ -337,6 +350,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeT1_GPeT1.connect(p=0.02)
         syn_GPeT1_GPeT1.w = 1
         syn_GPeT1_GPeT1.g0_g = self._get_param('g1g1_g0_g')
+        syn_GPeT1_GPeT1.gaba_beta = self._get_param('g1g1_gaba_beta')
         syn_GPeT1_GPeT1.tau_GABA = self._get_param('g1g1_gaba_tau_syn')
         syn_GPeT1_GPeT1.E_GABA = self._get_param('g1g1_gaba_E_rev')
         synapses.append(syn_GPeT1_GPeT1)
@@ -348,6 +362,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeT1_GPeTA.connect(p=0.02)
         syn_GPeT1_GPeTA.w = 1
         syn_GPeT1_GPeTA.g0_g = self._get_param('g1ga_g0_g')
+        syn_GPeT1_GPeTA.gaba_beta = self._get_param('g1ga_gaba_beta')
         syn_GPeT1_GPeTA.tau_GABA = self._get_param('g1ga_gaba_tau_syn')
         syn_GPeT1_GPeTA.E_GABA = self._get_param('g1ga_gaba_E_rev')
         synapses.append(syn_GPeT1_GPeTA)
@@ -359,6 +374,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeTA_GPeT1.connect(p=0.05)
         syn_GPeTA_GPeT1.w = 1
         syn_GPeTA_GPeT1.g0_g = self._get_param('gag1_g0_g')
+        syn_GPeTA_GPeT1.gaba_beta = self._get_param('gag1_gaba_beta')
         syn_GPeTA_GPeT1.tau_GABA = self._get_param('gag1_gaba_tau_syn')
         syn_GPeTA_GPeT1.E_GABA = self._get_param('gag1_gaba_E_rev')
         synapses.append(syn_GPeTA_GPeT1)
@@ -370,6 +386,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeTA_GPeTA.connect(p=0.02)
         syn_GPeTA_GPeTA.w = 1
         syn_GPeTA_GPeTA.g0_g = self._get_param('gaga_g0_g')
+        syn_GPeTA_GPeTA.gaba_beta = self._get_param('gaga_gaba_beta')
         syn_GPeTA_GPeTA.tau_GABA = self._get_param('gaga_gaba_tau_syn')
         syn_GPeTA_GPeTA.E_GABA = self._get_param('gaga_gaba_E_rev')
         synapses.append(syn_GPeTA_GPeTA)
@@ -381,6 +398,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeTA_FSN.connect(p=0.03)
         syn_GPeTA_FSN.w = 1
         syn_GPeTA_FSN.g0_g = self._get_param('gafs_g0_g')
+        syn_GPeTA_FSN.gaba_beta = self._get_param('gafs_gaba_beta')
         syn_GPeTA_FSN.tau_GABA = self._get_param('gafs_gaba_tau_syn')
         syn_GPeTA_FSN.E_GABA = self._get_param('gafs_gaba_E_rev')
         synapses.append(syn_GPeTA_FSN)
@@ -392,6 +410,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeTA_MSND1.connect(p=0.03)
         syn_GPeTA_MSND1.w = 1
         syn_GPeTA_MSND1.g0_g = self._get_param('gad1_g0_g')
+        syn_GPeTA_MSND1.gaba_beta = self._get_param('gad1_gaba_beta')
         syn_GPeTA_MSND1.tau_GABA = self._get_param('gad1_gaba_tau_syn')
         syn_GPeTA_MSND1.E_GABA = self._get_param('gad1_gaba_E_rev')
         synapses.append(syn_GPeTA_MSND1)
@@ -403,6 +422,7 @@ class GPe_STN_inh_ext_dop_dep(SynapseBase):
         syn_GPeTA_MSND2.connect(p=0.03)
         syn_GPeTA_MSND2.w = 1
         syn_GPeTA_MSND2.g0_g = self._get_param('gad2_g0_g')
+        syn_GPeTA_MSND1.gaba_beta = self._get_param('gad2_gaba_beta')
         syn_GPeTA_MSND2.tau_GABA = self._get_param('gad2_gaba_tau_syn')
         syn_GPeTA_MSND2.E_GABA = self._get_param('gad2_gaba_E_rev')
         synapses.append(syn_GPeTA_MSND2)
