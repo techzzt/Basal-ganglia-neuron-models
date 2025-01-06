f6ff6117f19292ceaf44f7c629e617a1a67f850d
Keun <49440852+techzzt@users.noreply.github.com>
2025-01-06 16:27:26 +0900

    Add files via upload

--- bg_insilico/Neuronmodels/FSN.py
+++ bg_insilico/Neuronmodels/FSN.py
@@ -1,40 +1,16 @@
+import importlib
 from brian2 import *
 
-# Define the equations for the neuron populations
-eqs = '''
-dv/dt = (k * 1 * pF/ms/mV * (v - vr) * (v - vt) - u * pF + I) / C : volt (unless refractory)
-du/dt = int(v <= vb) * (a * (b * (vb - v)**3 - u)) + int(v > vb) * (-a * u) : volt/second
-I = Ispon + Istim + Isyn : amp
-Istim   : amp
-Ispon   : amp
-Isyn = I_AMPA_FSN + I_NMDA + I_GABA_FSN: amp
-I_GABA_FSN  : amp
-I_AMPA_FSN : amp
-I_NMDA : amp
-a : 1/second
-b : volt**-2/second
-k : 1
-E_L    : volt
-vt     : volt
-vr     : volt 
-vb     : volt  
-tau_w  : second
-th     : volt
-C      : farad
-c      : volt
-d      : volt/second
-'''
+from module.models import QIF_FSN
 
 class NeuronModel:
     def __init__(self, N, params):
-        # Parse the parameters from the params dictionary
         super().__init__(N, params)
         self.neurons = None
 
     def create_neurons(self):
         raise NotImplementedError("Subclasses should implement this method.")
 
-# Class for the FSN neuron model
 class FSN(NeuronModel):
     def __init__(self, N, params):
         self.N = N
@@ -42,6 +18,8 @@ class FSN(NeuronModel):
         self.neurons = None
 
     def create_neurons(self):
+        eqs = QIF_FSN.eqs 
+
         self.neurons = NeuronGroup(
             self.N, eqs, threshold='v > th', reset='v = c; u += d', method='euler'
         )
@@ -53,7 +31,7 @@ class FSN(NeuronModel):
         self.neurons.vb = self.params['vb']['value'] * eval(self.params['vb']['unit'])
         self.neurons.k = self.params['k']['value']
         self.neurons.a = self.params['a']['value'] / second
-        self.neurons.b = self.params['b']['value'] / (volt**2 * second)  # 단위 변경
+        self.neurons.b = self.params['b']['value'] / (volt**2 * second)
         self.neurons.d = self.params['d']['value']
         self.neurons.C = self.params['C']['value'] * eval(self.params['C']['unit'])
         self.neurons.c = self.params['c']['value'] * eval(self.params['c']['unit'])
