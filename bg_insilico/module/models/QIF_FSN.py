30a502b43d4a7bf19783d65c735bb99948f752cc
Keun <49440852+techzzt@users.noreply.github.com>
2025-01-06 16:26:28 +0900

    Add files via upload

---
+++ bg_insilico/module/models/QIF_FSN.py
@@ -0,0 +1,26 @@
+# QIF.py
+from brian2 import *
+
+eqs = '''
+dv/dt = (k * 1 * pF/ms/mV * (v - vr) * (v - vt) - u * pF + I) / C : volt (unless refractory)
+du/dt = int(v <= vb) * (a * (b * (vb - v)**3 - u)) + int(v > vb) * (-a * u) : volt/second
+I = Ispon + Istim + Isyn : amp
+Istim   : amp
+Ispon   : amp
+Isyn = I_AMPA + I_NMDA + I_GABA: amp
+I_GABA : amp
+I_AMPA : amp
+I_NMDA : amp
+a : 1/second
+b : volt**-2/second
+k : 1
+E_L    : volt
+vt     : volt
+vr     : volt 
+vb     : volt  
+tau_w  : second
+th     : volt
+C      : farad
+c      : volt
+d      : volt/second
+'''
