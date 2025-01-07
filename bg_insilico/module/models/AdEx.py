82e329e3206e8b985aeec0a674ccfcec6aa83fcf
Keun <49440852+techzzt@users.noreply.github.com>
2025-01-07 14:53:26 +0900

    Add files via upload

---
+++ bg_insilico/module/models/AdEx.py
@@ -0,0 +1,25 @@
+from brian2 import *
+
+eqs = '''
+dv/dt = (-g_L * (v - E_L) + g_L * Delta_T * exp((v - vt) / Delta_T) - u + I) / C : volt
+du/dt = (a * (v - E_L) - u) / tau_w : amp
+I = Ispon + Istim + Isyn : amp
+Istim   : amp
+Ispon   : amp
+Isyn = I_AMPA_GPeT1 + I_NMDA + I_GABA_GPeT1: amp
+
+I_AMPA_GPeT1 : amp
+I_NMDA : amp
+I_GABA_GPeT1 : amp
+
+g_L    : siemens
+E_L    : volt
+Delta_T: volt
+vt     : volt
+vr     : volt 
+tau_w  : second
+th     : volt
+a      : siemens
+d      : amp
+C      : farad
+'''
---
+++ bg_insilico/module/models/QIF.py
@@ -0,0 +1,24 @@
+from brian2 import *
+
+eqs = '''
+dv / dt = (k*1*pF/ms/mV*(v-vr)*(v-vt) - u*pF + I) / C : volt (unless refractory)
+du/dt = a * (b * (v - vr) - u) : volt/second
+I = Ispon + Istim + Isyn : amp
+Istim   : amp
+Ispon   : amp
+Isyn = I_AMPA + I_NMDA + I_GABA: amp
+I_GABA  : amp
+I_AMPA : amp
+I_NMDA : amp
+a : 1/second
+b : 1/second
+k : 1
+E_L    : volt
+vt     : volt
+vr     : volt 
+tau_w  : second
+th     : volt
+c      : volt
+C      : farad
+d      : volt/second
+'''
