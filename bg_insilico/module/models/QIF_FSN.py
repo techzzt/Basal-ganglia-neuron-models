a05f585c186342f20f6bd3d48de4c7e99605ac8c
Jieun Kim (Keun) <49440852+techzzt@users.noreply.github.com>
2025-02-24 11:46:46 +0900

    Add files via upload

--- bg_insilico/module/models/QIF_FSN.py
+++ bg_insilico/module/models/QIF_FSN.py
@@ -1,24 +1,30 @@
-# QIF.py
 from brian2 import *
 
 eqs = '''
-dv/dt = (k * 1 * pF/ms/mV * (v - vr) * (v - vt) - u * pF + I - Isyn) / C : volt 
+dv/dt = (k * (1 * pF/ms/mV) * (v - vr) * (v - vt) - u * pF + I - Isyn) / C : volt
 du/dt = int(v <= vb) * (a * b * (v - vb)**3 / (volt**2 * second)) - int(v > vb) * (a * u) : volt/second
+
 I : amp
-Isyn = I_AMPA + I_NMDA + I_GABA : amp
-I_AMPA : amp
-I_NMDA : amp
-I_GABA : amp
+Isyn = I_AMPA + I_GABA : amp
+I_AMPA = g_a * (E_AMPA - v) : amp
+I_GABA = g_g * (E_GABA - v) : amp
+tau_GABA : second
+tau_AMPA : second
+dg_g/dt = -g_g / tau_GABA : siemens
+dg_a/dt = -g_a / tau_AMPA : siemens
+E_AMPA : volt
+E_GABA : volt
+Mg2 : 1
+
 a : 1/second
 b : 1
 k : 1
 E_L    : volt
 vt     : volt
-vr     : volt 
-vb     : volt  
-tau_w  : second
+vr     : volt
+vb     : volt
 th     : volt
-C      : farad
 c      : volt
+C      : farad
 d      : volt/second
 '''
