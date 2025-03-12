0f676851973aaa12b16d32ed454f0677973c3c60
Jieun Kim (Keun) <49440852+techzzt@users.noreply.github.com>
2025-03-12 16:41:17 +0900

    Add files via upload

--- bg_insilico/module/models/QIF_FSN.py
+++ bg_insilico/module/models/QIF_FSN.py
@@ -1,19 +1,29 @@
 from brian2 import *
 
 eqs = '''
-dv/dt = (k * (1 * pF/ms/mV) * (v - vr) * (v - vt) - u * pF + I - Isyn) / C : volt
+dv/dt = (k * (1 * pF/ms/mV) * (v - vr) * (v - vt) - u * pF + Isyn) / C : volt
 du/dt = int(v <= vb) * (a * b * (v - vb)**3 / (volt**2 * second)) - int(v > vb) * (a * u) : volt/second
 
-I : amp
-Isyn = I_AMPA + I_GABA : amp
+Isyn = I_AMPA + I_NMDA + I_GABA : amp
 I_AMPA = g_a * (E_AMPA - v) : amp
-I_GABA = g_g * (E_GABA - v) : amp
+I_GABA = gaba_beta * g_g * (E_GABA - v) : amp
+I_NMDA = nmda_beta * g_n * (E_NMDA - v) / (1 + Mg2 * exp(-0.062 * v / mV) / 3.57) : amp 
+
 tau_GABA : second
 tau_AMPA : second
+tau_NMDA : second
+
 dg_g/dt = -g_g / tau_GABA : siemens
 dg_a/dt = -g_a / tau_AMPA : siemens
+dg_n/dt = -g_n / tau_NMDA : siemens
+
 E_AMPA : volt
 E_GABA : volt
+E_NMDA : volt
+
+ampa_beta: 1
+gaba_beta: 1
+nmda_beta: 1
 Mg2 : 1
 
 a : 1/second
