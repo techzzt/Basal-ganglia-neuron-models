39ad8381448302bb197151c263f44a57712c911e
Jieun Kim (Keun) <49440852+techzzt@users.noreply.github.com>
2025-03-14 16:21:37 +0900

    Add files via upload

--- bg_insilico/module/models/QIF.py
+++ bg_insilico/module/models/QIF.py
@@ -1,20 +1,36 @@
 from brian2 import *
 
 eqs = '''
-dv / dt = (k*1*pF/ms/mV*(v-vr)*(v-vt) - u*pF + I - Isyn) / C : volt 
+dv / dt = (k*1*pF/ms/mV*(v-vr)*(v-vt) - u*pF + Isyn) / C : volt 
 du/dt = a * (b * (v - vr) - u) : volt/second
-I : amp
+
 Isyn = I_AMPA + I_NMDA + I_GABA : amp
-I_AMPA : amp
-I_NMDA : amp
-I_GABA : amp
+I_AMPA = g_a * (v - E_AMPA) : amp
+I_GABA = g_g * (v - E_GABA) : amp
+I_NMDA = nmda_beta * g_n * (E_NMDA - v) / (1 + Mg2 * exp(-0.062 * v / mV) / 3.57) : amp 
+
+tau_GABA : second
+tau_AMPA : second
+tau_NMDA : second
+
+dg_g/dt = -g_g / tau_GABA : siemens
+dg_a/dt = -g_a / tau_AMPA : siemens
+dg_n/dt = -g_n / tau_NMDA : siemens
+
+E_AMPA : volt
+E_GABA : volt
+E_NMDA : volt
+
+ampa_beta: 1
+gaba_beta: 1
+nmda_beta: 1
+Mg2 : 1
+
 a : 1/second
 b : 1/second
 k : 1
-E_L    : volt
 vt     : volt
 vr     : volt 
-tau_w  : second
 th     : volt
 c      : volt
 C      : farad
