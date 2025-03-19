3dad5c7da7fcdb2a38b4b94b652b4c19f99deb60
Jieun Kim (Keun) <49440852+techzzt@users.noreply.github.com>
2025-03-19 19:46:09 +0900

    Add files via upload

--- bg_insilico/Neuronmodels/MSND2.py
+++ bg_insilico/Neuronmodels/MSND2.py
@@ -2,7 +2,6 @@ import importlib
 from brian2 import *
 from module.models import QIF
 
-
 class NeuronModel:
     def __init__(self, N, params):
         super().__init__(N, params)
@@ -12,15 +11,26 @@ class NeuronModel:
         raise NotImplementedError("Subclasses should implement this method.")
 
 class MSND2(NeuronModel):
-    def __init__(self, N, params, receptor_params=None):
+    def __init__(self, N, params, connections=None):  # ✅ connections 추가
         self.N = N
         self.params = params
-        self.receptor_params = receptor_params
+        self.receptor_params = self.get_receptor_params(connections) if connections else {}
         self.neurons = None
+        print(f"[DEBUG] MSND2 receptor_params: {self.receptor_params}")  # 디버깅용 출력
+
+    def get_receptor_params(self, connections):
+        """현재 뉴런이 post-synaptic인 시냅스 파라미터를 connections에서 검색"""
+        receptor_params = {}
+        for conn_name, conn_data in connections.items():
+            if conn_data['post'] == "MSND2":  # ✅ MSND1 뉴런이 post라면 시냅스 정보 가져오기
+                receptor_params.update(conn_data.get('receptor_params', {}))
+        return receptor_params
 
     def create_neurons(self):
         eqs = QIF.eqs 
         self.neurons = NeuronGroup(self.N, eqs, threshold='v > th', reset='v = c; u += d', method='euler')
+
+        # 뉴런 파라미터 설정
         self.neurons.vr = self.params['vr']['value'] * eval(self.params['vr']['unit'])
         self.neurons.vt = self.params['vt']['value'] * eval(self.params['vt']['unit'])
         self.neurons.th = self.params['th']['value'] * eval(self.params['th']['unit'])
@@ -31,50 +41,37 @@ class MSND2(NeuronModel):
         self.neurons.C = self.params['C']['value'] * eval(self.params['C']['unit'])
         self.neurons.c = self.params['c']['value'] * eval(self.params['c']['unit'])
 
-        if 'receptor_params' in self.params:
-            rp = self.params['receptor_params']
-            # AMPA parameters
-            if 'AMPA' in rp:
-                self.neurons.E_AMPA = rp['AMPA']['E_rev']['value'] * eval(rp['AMPA']['E_rev']['unit'])
-                self.neurons.tau_AMPA = rp['AMPA']['tau_syn']['value'] * eval(rp['AMPA']['tau_syn']['unit'])
-                self.neurons.ampa_beta = rp['AMPA']['beta']['value']
-            else:
-                self.neurons.E_AMPA = 0 * mV
-                self.neurons.tau_AMPA = 1 * ms
-                self.neurons.ampa_beta = 0
+        # ✅ self.params가 아니라 self.receptor_params에서 시냅스 정보 가져오기
+        rp = self.receptor_params
 
-            # NMDA parameters
-            if 'NMDA' in rp:
-                self.neurons.E_NMDA = rp['NMDA']['E_rev']['value'] * eval(rp['NMDA']['E_rev']['unit'])
-                self.neurons.tau_NMDA = rp['NMDA']['tau_syn']['value'] * eval(rp['NMDA']['tau_syn']['unit'])
-                self.neurons.nmda_beta = rp['NMDA']['beta']['value']
-            else:
-                self.neurons.E_NMDA = 0 * mV
-                self.neurons.tau_NMDA = 1 * ms
-                self.neurons.nmda_beta = 0
-
-            # GABA parameters
-            if 'GABA' in rp:
-                self.neurons.E_GABA = rp['GABA']['E_rev']['value'] * eval(rp['GABA']['E_rev']['unit'])
-                self.neurons.tau_GABA = rp['GABA']['tau_syn']['value'] * eval(rp['GABA']['tau_syn']['unit'])
-                self.neurons.gaba_beta = rp['GABA']['beta']['value']
-            else:
-                self.neurons.E_GABA = 0 * mV
-                self.neurons.tau_GABA = 1 * ms
-                self.neurons.gaba_beta = 0
+        # AMPA parameters
+        if 'AMPA' in rp:
+            self.neurons.E_AMPA = rp['AMPA']['E_rev']['value'] * eval(rp['AMPA']['E_rev']['unit'])
+            self.neurons.tau_AMPA = rp['AMPA']['tau_syn']['value'] * eval(rp['AMPA']['tau_syn']['unit'])
+            self.neurons.ampa_beta = rp['AMPA']['beta']['value']
         else:
-            # defaults (convert to zero)
             self.neurons.E_AMPA = 0 * mV
             self.neurons.tau_AMPA = 1 * ms
             self.neurons.ampa_beta = 0
+
+        # NMDA parameters
+        if 'NMDA' in rp:
+            self.neurons.E_NMDA = rp['NMDA']['E_rev']['value'] * eval(rp['NMDA']['E_rev']['unit'])
+            self.neurons.tau_NMDA = rp['NMDA']['tau_syn']['value'] * eval(rp['NMDA']['tau_syn']['unit'])
+            self.neurons.nmda_beta = rp['NMDA']['beta']['value']
+        else:
             self.neurons.E_NMDA = 0 * mV
             self.neurons.tau_NMDA = 1 * ms
             self.neurons.nmda_beta = 0
+
+        # GABA parameters
+        if 'GABA' in rp:
+            self.neurons.E_GABA = rp['GABA']['E_rev']['value'] * eval(rp['GABA']['E_rev']['unit'])
+            self.neurons.tau_GABA = rp['GABA']['tau_syn']['value'] * eval(rp['GABA']['tau_syn']['unit'])
+            self.neurons.gaba_beta = rp['GABA']['beta']['value']
+        else:
             self.neurons.E_GABA = 0 * mV
             self.neurons.tau_GABA = 1 * ms
             self.neurons.gaba_beta = 0
-        
-        return self.neurons
 
-    
-    
+        return self.neurons
--- bg_insilico/Neuronmodels/STN.py
+++ bg_insilico/Neuronmodels/STN.py
@@ -14,14 +14,22 @@ class NeuronModel:
 
     def create_neurons(self):
         raise NotImplementedError("Subclasses should implement this method.")
-    
 
 class STN(NeuronModel):
-    def __init__(self, N, params, receptor_params=None):
+    def __init__(self, N, params, connections=None):  # ✅ connections 추가
         self.N = N
         self.params = params
-        self.receptor_params = receptor_params
+        self.receptor_params = self.get_receptor_params(connections) if connections else {}
         self.neurons = None
+        print(f"[DEBUG] STN receptor_params: {self.receptor_params}")  # 디버깅용 출력
+
+    def get_receptor_params(self, connections):
+        """현재 뉴런이 post-synaptic인 시냅스 파라미터를 connections에서 검색"""
+        receptor_params = {}
+        for conn_name, conn_data in connections.items():
+            if conn_data['post'] == "STN":  # ✅ STN 뉴런이 post라면 시냅스 정보 가져오기
+                receptor_params.update(conn_data.get('receptor_params', {}))
+        return receptor_params
 
     def create_neurons(self):
         eqs = AdEx.eqs
@@ -34,7 +42,7 @@ class STN(NeuronModel):
             self.N, eqs, threshold='v > th', reset=reset, method='euler'
         )
 
-        # Assign basic neuron parameters from the "other" parameters in JSON.
+        # 뉴런 파라미터 설정
         self.neurons.g_L = self.params['g_L']['value'] * eval(self.params['g_L']['unit'])
         self.neurons.E_L = self.params['E_L']['value'] * eval(self.params['E_L']['unit'])
         self.neurons.Delta_T = self.params['Delta_T']['value'] * eval(self.params['Delta_T']['unit'])
@@ -45,49 +53,38 @@ class STN(NeuronModel):
         self.neurons.a = self.params['a']['value'] * eval(self.params['a']['unit'])
         self.neurons.d = self.params['d']['value'] * eval(self.params['d']['unit'])
         self.neurons.C = self.params['C']['value'] * eval(self.params['C']['unit'])
-       
-        if 'receptor_params' in self.params:
-            rp = self.params['receptor_params']
-            # AMPA parameters
-            if 'AMPA' in rp:
-                self.neurons.E_AMPA = rp['AMPA']['E_rev']['value'] * eval(rp['AMPA']['E_rev']['unit'])
-                self.neurons.tau_AMPA = rp['AMPA']['tau_syn']['value'] * eval(rp['AMPA']['tau_syn']['unit'])
-                self.neurons.ampa_beta = rp['AMPA']['beta']['value']
-            else:
-                self.neurons.E_AMPA = 0 * mV
-                self.neurons.tau_AMPA = 1 * ms
-                self.neurons.ampa_beta = 0
 
-            # NMDA parameters
-            if 'NMDA' in rp:
-                self.neurons.E_NMDA = rp['NMDA']['E_rev']['value'] * eval(rp['NMDA']['E_rev']['unit'])
-                self.neurons.tau_NMDA = rp['NMDA']['tau_syn']['value'] * eval(rp['NMDA']['tau_syn']['unit'])
-                self.neurons.nmda_beta = rp['NMDA']['beta']['value']
-            else:
-                self.neurons.E_NMDA = 0 * mV
-                self.neurons.tau_NMDA = 1 * ms
-                self.neurons.nmda_beta = 0
+        # ✅ self.params가 아니라 self.receptor_params에서 시냅스 정보 가져오기
+        rp = self.receptor_params
 
-            # GABA parameters
-            if 'GABA' in rp:
-                self.neurons.E_GABA = rp['GABA']['E_rev']['value'] * eval(rp['GABA']['E_rev']['unit'])
-                self.neurons.tau_GABA = rp['GABA']['tau_syn']['value'] * eval(rp['GABA']['tau_syn']['unit'])
-                self.neurons.gaba_beta = rp['GABA']['beta']['value']
-            else:
-                self.neurons.E_GABA = 0 * mV
-                self.neurons.tau_GABA = 1 * ms
-                self.neurons.gaba_beta = 0
+        # AMPA parameters
+        if 'AMPA' in rp:
+            self.neurons.E_AMPA = rp['AMPA']['E_rev']['value'] * eval(rp['AMPA']['E_rev']['unit'])
+            self.neurons.tau_AMPA = rp['AMPA']['tau_syn']['value'] * eval(rp['AMPA']['tau_syn']['unit'])
+            self.neurons.ampa_beta = rp['AMPA']['beta']['value']
         else:
-            # defaults (convert to zero)
             self.neurons.E_AMPA = 0 * mV
             self.neurons.tau_AMPA = 1 * ms
             self.neurons.ampa_beta = 0
+
+        # NMDA parameters
+        if 'NMDA' in rp:
+            self.neurons.E_NMDA = rp['NMDA']['E_rev']['value'] * eval(rp['NMDA']['E_rev']['unit'])
+            self.neurons.tau_NMDA = rp['NMDA']['tau_syn']['value'] * eval(rp['NMDA']['tau_syn']['unit'])
+            self.neurons.nmda_beta = rp['NMDA']['beta']['value']
+        else:
             self.neurons.E_NMDA = 0 * mV
             self.neurons.tau_NMDA = 1 * ms
             self.neurons.nmda_beta = 0
+
+        # GABA parameters
+        if 'GABA' in rp:
+            self.neurons.E_GABA = rp['GABA']['E_rev']['value'] * eval(rp['GABA']['E_rev']['unit'])
+            self.neurons.tau_GABA = rp['GABA']['tau_syn']['value'] * eval(rp['GABA']['tau_syn']['unit'])
+            self.neurons.gaba_beta = rp['GABA']['beta']['value']
+        else:
             self.neurons.E_GABA = 0 * mV
             self.neurons.tau_GABA = 1 * ms
             self.neurons.gaba_beta = 0
-        
+
         return self.neurons
-    
