480408b49b5273917bec94975970677731f4be30
Jieun Kim (Keun) <49440852+techzzt@users.noreply.github.com>
2025-05-28 17:22:06 +0900

    Add files via upload

--- bg_insilico/run.py
+++ bg_insilico/run.py
@@ -5,13 +5,14 @@ from module.utils.param_loader import load_params
 from brian2 import ms 
 
 def main():
-    params_file = 'config/test_normal_msn_params_noin.json'
+    params_file = 'config/test_normal_fsn_params_noin.json'
     
     params = load_params(params_file)
     neuron_configs = params['neurons']
     connections = params['connections']   
+    synapse_class = params['synapse_class']
     simulation_params = params['simulation']
-    plot_order = params.get('plot_order', [])
+    plot_order = params['plot_order']
     start_time = params.get('start_time', 0) * ms
     end_time = params.get('end_time', 10000) * ms
 
@@ -19,8 +20,12 @@ def main():
     results = run_simulation_with_inh_ext_input(
         neuron_configs=neuron_configs,
         connections=connections,
-        simulation_params=simulation_params
-    )
+        synapse_class=synapse_class,
+        simulation_params=simulation_params,    
+        plot_order=plot_order, 
+        start_time=start_time,
+        end_time=end_time
+        )
     
     print("Simulation completed successfully")
 
